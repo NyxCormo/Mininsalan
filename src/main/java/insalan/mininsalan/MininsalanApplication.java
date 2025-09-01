8d49e78b5dfa800b2ec820296c6dc76bdaddc43e
ClementLaine <clement.laine@insa-rennes.fr>
2025-09-01 10:34:41 +0200

    initial commit

---
+++ src/main/java/insalan/mininsalan/MininsalanApplication.java
@@ -0,0 +1,13 @@
+package insalan.mininsalan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MininsalanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MininsalanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/insalan/mininsalan/MininsalanApplicationTests.java
@@ -0,0 +1,13 @@
+package insalan.mininsalan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MininsalanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
